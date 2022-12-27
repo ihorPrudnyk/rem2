1f3986a1d3ec7b175b6e3411912429cc715102bd
ihorPrudnyk <prudnyk.iv@gmail.com>
2022-12-27 12:43:54 +0000

    івіфф

--- rem2/ViewController.swift
+++ rem2/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var lableValue: UILabel!
+    @IBOutlet weak var helloLableValue: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var colorValue: UIView!
+    let defaultColorValue = UIColor.lightGray
+    
+    @IBAction func pressedHelloLable(_ sender: Any) {
+        helloLableValue.text = "Ти де Мартін?"
+    }
+    
+    @IBAction func switchImageValue(_ sender: UISwitch) {
+        image.isHidden = !sender.isOn
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloLableValue.text = " "
+        colorValue.backgroundColor = defaultColorValue
+        
+      
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = (slider.maximumValue - slider.minimumValue) / 2 + slider.minimumValue
+        updateSliderValue()
+    }
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        updateSliderValue()
+    }
+    func updateSliderValue(){
+        lableValue.text = "\(Int(slider.value))"
+    }
+    @IBAction func redColorPressed(_ sender: Any) {
+        colorValue.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func blueColorPressed(_ sender: Any) {
+        colorValue.backgroundColor = UIColor.blue
     }
 
 
+    @IBAction func greenColorPressed(_ sender: Any) {
+        colorValue.backgroundColor = UIColor.green
+    }
+    @IBAction func resetColorPressed(_ sender: Any) {
+        colorValue.backgroundColor = defaultColorValue
+    }
+    
 }
 
